7aaeb6f6fe5d4d264b8283c2085bc2aee68bd69b
Tony Vu <tony.vu@puppetlabs.com>
2017-01-23 15:23:18 -0800

    Revert "(BKR-1044) add in boiler-plate include mechanism"
    This reverts commit 3b9ae70c6848c1218bb3b888855687ec8e496fd2.

--- lib/beaker-facter.rb
+++ lib/beaker-facter.rb
@@ -9,17 +9,3 @@ module Beaker
     end
   end
 end
-
-# Boilerplate DSL inclusion mechanism:
-# First we register our module with the Beaker DSL
-Beaker::DSL.register( Beaker::DSL::Helpers::Facter )
-
-# Second,We need to reload the DSL, but before we had reloaded
-# it in the global namespace, which result in errors colliding
-# with other gems rightfully not expecting beaker's dsl to
-# be available at the global level.
-module Beaker
-  class TestCase
-    include Beaker::DSL
-  end
-end
